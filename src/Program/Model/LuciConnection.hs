2854ec66db9e533c476e2ef85e8a004a9c9b9d4e
achirkin <achirkin@users.noreply.github.com>
2016-10-26 20:35:46 +0200

    Working on flexible visual service

--- src/Program/Model/LuciConnection.hs
+++ src/Program/Model/LuciConnection.hs
@@ -9,6 +9,7 @@
 --
 --
 -----------------------------------------------------------------------------
+{-# LANGUAGE DataKinds, FlexibleInstances, MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecursiveDo #-}
 module Program.Model.LuciConnection
@@ -21,9 +22,11 @@ module Program.Model.LuciConnection
 --import Control.Arrow (second)
 --import Data.Geometry
 --import JsHs
-import JsHs.JSString (pack) -- JSString, append
+import JsHs.JSString (pack, unpack') -- JSString, append
 --import Control.Monad (void, when)
 import JsHs.Useful
+import JsHs
+import JsHs.Types.Prim (jsNull)
 --import Text.Read (readMaybe)
 --import Data.Coerce
 import qualified JsHs.Array as JS
@@ -37,8 +40,9 @@ import Unsafe.Coerce (unsafeCoerce)
 
 import Data.Maybe (fromMaybe)
 import Data.Monoid ((<>))
-import Data.Time (UTCTime)
+import Data.Time
 import Data.Time.Format (formatTime, defaultTimeLocale)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime, utcTimeToPOSIXSeconds)
 import Reactive.Banana.Frameworks
 import Reactive.Banana.Combinators
 
@@ -63,16 +67,16 @@ luciBehavior lsettings geoJSONImportFire cityB groundUpdatedE
              geoJSONImportE clearGeometryE motionRecordsE = mdo
 
       -- Luci Client testing
-      (_luciClientB, luciClientE, runLuciService) <- luciHandler (fromMaybe "" $ luciRoute lsettings)
+      (luciClientB,noCallIdMsgE,unknownMsgE) <- luciHandler (fromMaybe "" $ luciRoute lsettings)
 
-      -- actions to do when luci state changes
-      let doLuciAction LuciClientOpening = logText' "Opening connection."
-          doLuciAction LuciClientClosed = logText' "LuciClient WebSocket connection closed."
-          doLuciAction (LuciClientError err) = logText' $ "LuciClient error: " <> err
-          doLuciAction _luciClient = logText' "Luci is ready"
-            -- TODO: run runQuaServiceList
---            sendMessage luciClient runQuaServiceList
-      reactimate $ doLuciAction <$> luciClientE
+--      -- actions to do when luci state changes
+--      let doLuciAction LuciClientOpening = logText' "Opening connection."
+--          doLuciAction LuciClientClosed = logText' "LuciClient WebSocket connection closed."
+--          doLuciAction (LuciClientError err) = logText' $ "LuciClient error: " <> err
+--          doLuciAction _luciClient = logText' "Luci is ready"
+--            -- TODO: run runQuaServiceList
+----            sendMessage luciClient runQuaServiceList
+--      reactimate $ doLuciAction <$> luciClientE
 
       -- general response to luci messages
       -- get scenario
@@ -85,12 +89,15 @@ luciBehavior lsettings geoJSONImportFire cityB groundUpdatedE
       let gotScenarioListF (SRResult _ scenarioList _) = displayScenarios scenarioList
           gotScenarioListF (SRError _ err) = logText' err
           gotScenarioListF _ = return ()
-      execute (runScenarioList runLuciService <$ getScListE) >>= switchE >>= reactimate . fmap gotScenarioListF
+      runScenarioList luciClientB (() <$ getScListE) >>= reactimate . fmap gotScenarioListF
+--      callLuci (("scenario.GetList", [], []) <$ getScListE) >>= reactimate . fmap gotScenarioListF
+--      execute (runScenarioList runLuciService <$ getScListE) >>= switchE >>= reactimate . fmap gotScenarioListF
 
       -- asking luci to save a scenario on button click
       (askSaveScenarioE, onAskSaveScenarioFire) <- newEvent
       liftIO $ GUI.registerSaveScenario onAskSaveScenarioFire
-      scenarioSavedE <- execute ((\ci s -> runScenarioCreate runLuciService s (storeCityAsIs ci)) <$> cityB <@> askSaveScenarioE) >>= switchE
+      scenarioSavedE <- runScenarioCreate luciClientB $ (\ci s -> (s, storeCityAsIs ci)) <$> cityB <@> askSaveScenarioE
+--      scenarioSavedE <- execute ((\ci s -> runScenarioCreate runLuciService s (storeCityAsIs ci)) <$> cityB <@> askSaveScenarioE) >>= switchE
       scenarioSyncE_create <- mapEventIO id
            $ (\s -> do
               GUI.toggleSaveScenarioButton False s
@@ -106,7 +113,8 @@ luciBehavior lsettings geoJSONImportFire cityB groundUpdatedE
       (askForScenarioE, onAskForScenarioFire) <- newEvent
       liftIO $ registerAskLuciForScenario (curry onAskForScenarioFire)
       -- Asking luci for a scenario on button click
-      gotScenarioE <- execute (runScenarioGet runLuciService . fst <$> askForScenarioE) >>= switchE
+      gotScenarioE <- runScenarioGet luciClientB (fst <$> askForScenarioE)
+--      gotScenarioE <- execute (runScenarioGet runLuciService . fst <$> askForScenarioE) >>= switchE
       let gotScenarioF (SRResult _ (LuciResultScenario scId fc t) _) = geoJSONImportFire (Right fc) >> onScenarioGetFire (scId, t)
           gotScenarioF (SRError _ err) = logText' err
           gotScenarioF _ = return ()
@@ -144,44 +152,252 @@ luciBehavior lsettings geoJSONImportFire cityB groundUpdatedE
           toggleSaveScenarioA (SSSynced _ s t) = GUI.toggleSaveScenarioButton False
                                                  ("[" <> ScenarioName (pack $ formatTime defaultTimeLocale "%y.%m.%d-%H:%M:%S" t) <> "] " <> s)
 
-          updateScenarioA (SSSynced sid _ _) ci gId = runScenarioUpdate runLuciService sid (storeObjectsAsIs [gId] ci)
-          updateScenarioA _ _ _ = return never
-
+--          updateScenarioA (SSSynced sid _ _) ci gId = runScenarioUpdate runLuciService sid (storeObjectsAsIs [gId] ci)
+--          updateScenarioA _ _ _ = return never
+          updateScenarioF (SSSynced sid _ _) ci gId = Just (sid, storeObjectsAsIs [gId] ci)
+          updateScenarioF _ _ _ = Nothing
+--
           serviceRunsE = filterJust $ serviceRunsF <$> scenarioSyncB <@> groundUpdatedE
-          serviceRunsF (SSSynced sid _ _) (GroundUpdated points) = Just $ VisualServiceRunPoints sid [] [] [] (fromJSArrayToTypedArray $ PS.flatten points)
+          serviceRunsF (SSSynced sid _ _) (GroundUpdated points) = Just $ VisualServiceRunPoints sid (fromJSArrayToTypedArray $ PS.flatten points)
           serviceRunsF _ _ = Nothing
 
           serviceButtonF GroundUpdated{} = GUI.toggleServiceClear True
           serviceButtonF GroundCleared{} = GUI.toggleServiceClear False
-
-
-          askSubscribeForScenario SSSynced{} SSSynced{} = return never
-          askSubscribeForScenario _ (SSSynced sid _ _) = runScenarioSubscribe runLuciService sid
-          askSubscribeForScenario _ _ = return never
+--
+--
+--          askSubscribeForScenario SSSynced{} SSSynced{} = return never
+--          askSubscribeForScenario _ (SSSynced sid _ _) = runScenarioSubscribe runLuciService sid
+--          askSubscribeForScenario _ _ = return never
+          -- TODO: Something is wrong with this logic, need to rethink
+          askSubscribeForScenario SSSynced{} SSSynced{} = Nothing
+          askSubscribeForScenario _ (SSSynced sid _ _) = Just sid
+          askSubscribeForScenario _ _ = Nothing
 
       -- show reflect scenario sync state
       reactimate $ toggleSaveScenarioA <$> scenarioSyncE
 
 
-      -- sync geometry with Luci
+--      -- sync geometry with Luci
       lateObjectRecordsE <- execute $ return . fst <$> motionRecordsE
-      scenarioOutUpdatesE <- execute (updateScenarioA <$> scenarioSyncB <*> cityB <@> lateObjectRecordsE) >>= switchE
-      scenarioInUpdatesE <- execute (askSubscribeForScenario <$>  scenarioSyncB <@> scenarioSyncE_obtained) >>= switchE
---      reactimate $ gotScenarioF <$> scenarioInUpdatesE
-      let (errsOfScOutE, _, _) = catResponses scenarioOutUpdatesE
-          (errsOfScInE, _, _) = catResponses scenarioInUpdatesE
+      sendScenarioUpdateE <- runScenarioUpdate luciClientB . filterJust $ updateScenarioF <$> scenarioSyncB <*> cityB <@> lateObjectRecordsE
+      receiveScenarioUpdateE <- runScenarioSubscribe luciClientB . filterJust $ askSubscribeForScenario <$> scenarioSyncB <@> scenarioSyncE_obtained
+      let (noCallIdMsgE', subscribeToUpdatesE)
+                               = split $ (\m -> if 0 == responseCallId m
+                                                then Right m
+                                                else Left m
+                                         ) <$> noCallIdMsgE
+      reactimate $ gotScenarioF . fmap (JS.asLikeJS . srVal) <$> subscribeToUpdatesE
+--      scenarioOutUpdatesE <- execute (updateScenarioA <$> scenarioSyncB <*> cityB <@> lateObjectRecordsE) >>= switchE
+--      scenarioInUpdatesE <- execute (askSubscribeForScenario <$>  scenarioSyncB <@> scenarioSyncE_obtained) >>= switchE
+--      reactimate $ gotScenarioF <$> receiveScenarioUpdateE
+      let (errsOfScOutE, _, _) = catResponses sendScenarioUpdateE
+          (errsOfScInE, _, _) = catResponses receiveScenarioUpdateE
       reactimate $ logText' <$> errsOfScInE
       reactimate $ logText' <$> errsOfScOutE
 
       -- run luci service!
---      (_vsManagerB, vsResultsE) <- vsManagerBehavior serviceFinishE
-      (vsErrorsR, vsResultsE) <- fmap split $
-                    execute (runVService runLuciService (VisualService "DistanceToWalls") <$> serviceRunsE) -- GenericIsovistService
-                       >>= switchE
-      reactimate $ (\t -> logText' t >> GUI.toggleServiceClear False) <$> vsErrorsR
+      -- vsManagerBehavior ("DistanceToWalls" <$ serviceRunsE)
+      (selectServiceE, selectServiceFire) <- newEvent
+      (updateSListE, updateSListFire) <- newEvent
+      (changeSParamE, changeSParamFire) <- newEvent
+      (vsManagerB, vsResultsE) <-vsManagerBehavior selectServiceE changeSParamE updateSListE
+      -- TODO: delete following block!
+      (triggerQuaServiceListE,triggerQuaServiceListF) <- newEvent
+      serviceListUpdateE <- runQuaServiceList luciClientB triggerQuaServiceListE
+      let serviceListUpdateA (SRResult _ r@(ServiceList jsarray) _) = do
+            print $ JS.toList jsarray
+            updateSListFire r
+            case ServiceName <$> JS.toList jsarray of
+              [] -> return ()
+              (x:_) -> selectServiceFire x
+          serviceListUpdateA (SRError _ e) = logText' e
+          serviceListUpdateA _ = return ()
+      reactimate $ serviceListUpdateA <$> serviceListUpdateE
+      reactimate $ triggerQuaServiceListF <$> (() <$ gotScenarioE)
+
+
+      runVService vsManagerB luciClientB serviceRunsE
+
+
+--      reactimate $ (\t -> logText' t >> GUI.toggleServiceClear False) <$> vsErrsE
+--      reactimate $ logText' "Service started" <$ vsProgresE
+--      reactimate $ logText' "Service finished" <$ vsResultsE
       reactimate $ serviceButtonF <$> groundUpdatedE
 
+      reactimate $ (\(msg, _) -> print $ "Ignoring message: " ++ (unpack' . jsonStringify $ JS.asJSVal msg)) <$> unknownMsgE
+      reactimate $ (\msg -> print $ "Ignoring message: " ++ show (fmap (unpack' . jsonStringify . srVal) msg)) <$> noCallIdMsgE'
+
+
       return vsResultsE
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Pre-defined messages
+----------------------------------------------------------------------------------------------------
+
+--runHelper :: JS.LikeJS s a => ServiceName -> [(JSString, JSVal)] -> ServiceInvocation -> MomentIO (Event (Either JSString a))
+--runHelper sname pams run = filterJust . fmap f <$> run sname pams []
+--  where
+--    f (SRResult _ (ServiceResult res) _) = Just . Right $ JS.asLikeJS res
+--    f SRProgress{} = Nothing
+--    f (SRError _ s) = Just $ Left s
+
+-- | A message to get list of available services from luci
+runServiceList :: Behavior LuciClient -> Event () -> MomentIO (Event (ServiceResponse LuciResultServiceList))
+runServiceList lcB e = runService lcB $ ("ServiceList",[],[]) <$ e
+
+
+---- | run a testing service test.Fibonacci
+--runTestFibonacci :: Int -> LuciMessage
+--runTestFibonacci n = toLuciMessage (MsgRun "test.Fibonacci" [("amount", JS.asJSVal n)]) []
+
+--newtype LuciResultTestFibonacci = TestFibonacci [Int]
+--  deriving (Show, Eq)
+--instance LikeJS "Object" LuciResultTestFibonacci where
+--  asLikeJS b = case getProp "fibonacci_sequence" b of
+--                 Just x  -> TestFibonacci $ JS.asLikeJS x
+--                 Nothing -> TestFibonacci []
+--  asJSVal (TestFibonacci xs) = setProp "fibonacci_sequence" xs newObj
+
+
+
+-- | Luci scenario
+data LuciScenario = LuciResultScenario ScenarioId FeatureCollection UTCTime
+instance JS.LikeJS "Object" LuciScenario where
+  asLikeJS jsv = case (,) <$> getProp "ScID" jsv <*> getProp "FeatureCollection" jsv of
+                  Just (scId, fc) -> LuciResultScenario scId fc t
+                  Nothing -> anotherTry
+     where
+       t = posixSecondsToUTCTime . realToFrac . secondsToDiffTime . fromMaybe 0 $ getProp "lastmodified" jsv
+       anotherTry = LuciResultScenario (fromMaybe 0 $ getProp "ScID" jsv)
+              (fromMaybe (JS.fromJSArray JS.emptyArray) (getProp "geometry_output" jsv >>= getProp "geometry")) t
+  asJSVal (LuciResultScenario scId fc _) =
+            setProp "ScID"  (JS.asJSVal scId)
+          $ setProp "FeatureCollection" fc newObj
+
+-- | Luci scenario
+data LuciScenarioCreated = LuciResultScenarioCreated ScenarioId UTCTime
+instance JS.LikeJS "Object" LuciScenarioCreated where
+  asLikeJS jsv = LuciResultScenarioCreated (fromMaybe 0 $ getProp "ScID" jsv)
+                                     (posixSecondsToUTCTime . realToFrac . secondsToDiffTime . fromMaybe 0 $ getProp "lastmodified" jsv)
+  asJSVal (LuciResultScenarioCreated scId lm) =
+            setProp "ScID"  (JS.asJSVal scId)
+          $ setProp "lastmodified" (round $ utcTimeToPOSIXSeconds lm :: Int) newObj
+
+-- | Pass the name of the scenario and a feature collection with geometry
+runScenarioCreate :: Behavior LuciClient
+                  -> Event
+                     ( ScenarioName -- ^ name of the scenario
+                     , FeatureCollection -- ^ content of the scenario
+                     )
+                  -> MomentIO (Event (ServiceResponse LuciScenarioCreated))
+runScenarioCreate lcB e = runService lcB $ (\v -> ("scenario.geojson.Create", f v, [])) <$> e
+  where
+    f (name, collection) =
+      [ ("name", JS.asJSVal name)
+      , ("geometry_input"
+        ,   setProp "format"  ("GeoJSON" :: JSString)
+          $ setProp "geometry" collection newObj
+        )
+      ]
+-- returns: "{"created":1470932237,"lastmodified":1470932237,"name":"dgdsfg","ScID":4}"
+
+
+runScenarioUpdate :: Behavior LuciClient
+                  -> Event
+                     ( ScenarioId -- ^ id of the scenario
+                     , FeatureCollection -- ^ content of the scenario update
+                     )
+                  -> MomentIO (Event (ServiceResponse JSVal))
+runScenarioUpdate lcB e = runService lcB $ (\v -> ("scenario.geojson.Update", f v, [])) <$> e
+  where
+    f (scId, collection) =
+      [ ("ScID", JS.asJSVal scId)
+      , ("geometry_input"
+        ,   setProp "format"  ("GeoJSON" :: JSString)
+          $ setProp "geometry" collection newObj
+        )
+      ]
+
+
+runScenarioGet :: Behavior LuciClient
+               -> Event ScenarioId -- ^ id of the scenario
+               -> MomentIO (Event (ServiceResponse LuciScenario))
+runScenarioGet lcB e = runService lcB $ (\v -> ("scenario.geojson.Get", f v, [])) <$> e
+  where
+    f scId =
+      [ ("ScID", JS.asJSVal scId)
+      ]
+
+-- returns: "{"lastmodified":1470932237,"ScID":4}"
+
+runScenarioSubscribe :: Behavior LuciClient
+                     -> Event ScenarioId -- ^ id of the scenario
+                     -> MomentIO (Event (ServiceResponse LuciScenario))
+runScenarioSubscribe lcB e = runService lcB $ (\v -> ("scenario.SubscribeTo", f v, [])) <$> e
+  where
+    f scId =
+      [ ("ScIDs", JS.asJSVal [scId])
+      , ("format", JS.asJSVal ("geojson" :: JSString))
+      ]
+
+
+runScenarioList :: Behavior LuciClient -> Event () -> MomentIO (Event (ServiceResponse ServiceResult))
+runScenarioList lcB e = runService lcB $ ("scenario.GetList",[],[]) <$ e
+
+
+newtype LuciResultScenarioList = ScenarioList [ScenarioDescription]
+  deriving (Show)
+instance JS.LikeJS "Object" LuciResultScenarioList where
+  asLikeJS b = case getProp "scenarios" b of
+                 Just x  -> ScenarioList x
+                 Nothing -> ScenarioList []
+  asJSVal (ScenarioList v) = setProp "scenarios" v newObj
+
+
+data ScenarioDescription = ScenarioDescription
+  { scCreated  :: UTCTime
+  , scModified :: UTCTime
+  , scName     :: ScenarioName
+  , sscId      :: ScenarioId
+  }
+  deriving (Eq,Ord,Show)
+instance JS.LikeJS "Object" ScenarioDescription where
+  asLikeJS jsv = ScenarioDescription
+    { scCreated  = f $ getProp "created" jsv
+    , scModified = f $ getProp "lastmodified" jsv
+    , scName     = fromMaybe "" $ getProp "name" jsv
+    , sscId      = fromMaybe (-1) $ getProp "ScID" jsv
+    }
+      where
+        f = posixSecondsToUTCTime . realToFrac . secondsToDiffTime . fromMaybe 0
+  asJSVal scd =
+          setProp "ScID" (sscId scd) . setProp "name" (scName scd)
+        . setProp "lastmodified" (f $ scModified scd :: Int) $ setProp "created" (f $ scCreated scd :: Int) newObj
+      where
+        f = round . utcTimeToPOSIXSeconds
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Helpers
+----------------------------------------------------------------------------------------------------
+
+
+
+
 
 
 
--- src/Program/Types.hs
+++ src/Program/Types.hs
@@ -20,6 +20,7 @@ import Data.Hashable
 import Data.String (IsString())
 import JsHs
 import JsHs.JSString (unpack')
+import GHC.Exts (IsString(..))
 
 newtype RequireViewUpdate a = RequireViewUpdate a
 
@@ -43,7 +44,6 @@ data ScenarioSync
     -- ^ Run "scenario.geojson.Get", but not received result yet
   | SSSynced ScenarioId ScenarioName UTCTime
     -- ^ Last synchronized at a given time
-
 
 
 -- | Luci geomID is used to reference scenario objects
@@ -81,3 +81,83 @@ data WantPicture = WantPicture
 
 newtype PictureVal = PictureVal JSVal
 instance LikeJS "Object" PictureVal
+
+
+data RangedInt = RangedInt
+  { riMin :: !Int
+  , riMax :: !Int
+  , riVal :: !Int
+  }
+  deriving Show
+
+instance Eq RangedInt where
+  a == b = riVal a == riVal b
+
+instance Ord RangedInt where
+  a `compare` b = riVal a `compare` riVal b
+  a < b = riVal a < riVal b
+  a <= b = riVal a <= riVal b
+  a > b = riVal a > riVal b
+  a >= b = riVal a >= riVal b
+
+{-# INLINE riCombineSpreading #-}
+riCombineSpreading :: (Int -> Int -> Int) -> RangedInt -> RangedInt -> RangedInt
+riCombineSpreading f (RangedInt al ah a)  (RangedInt bl bh b) = RangedInt l h (min h . max l $ f a b)
+  where
+    l = min al bl
+    h = max ah bh
+
+instance Num RangedInt where
+  (+) = riCombineSpreading (+)
+  (-) = riCombineSpreading (-)
+  (*) = riCombineSpreading (*)
+  abs (RangedInt l h x) = RangedInt l h (min h . max l $ abs x)
+  negate (RangedInt l h x) = RangedInt l h (min h . max l $ negate x)
+  signum (RangedInt l h x) = RangedInt l h (min h . max l $ signum x)
+  fromInteger i = RangedInt minBound maxBound (fromInteger i)
+
+
+data RangedFloat = RangedFloat
+  { rfMin :: !Float
+  , rfMax :: !Float
+  , rfVal :: !Float
+  }
+  deriving Show
+
+instance Eq RangedFloat where
+  a == b = rfVal a == rfVal b
+
+instance Ord RangedFloat where
+  a `compare` b = rfVal a `compare` rfVal b
+  a < b = rfVal a < rfVal b
+  a <= b = rfVal a <= rfVal b
+  a > b = rfVal a > rfVal b
+  a >= b = rfVal a >= rfVal b
+
+{-# INLINE rfCombineSpreading #-}
+rfCombineSpreading :: (Float -> Float -> Float) -> RangedFloat -> RangedFloat -> RangedFloat
+rfCombineSpreading f (RangedFloat al ah a)  (RangedFloat bl bh b) = RangedFloat l h (min h . max l $ f a b)
+  where
+    l = min al bl
+    h = max ah bh
+
+instance Num RangedFloat where
+  (+) = rfCombineSpreading (+)
+  (-) = rfCombineSpreading (-)
+  (*) = rfCombineSpreading (*)
+  abs (RangedFloat l h x) = RangedFloat l h (min h . max l $ abs x)
+  negate (RangedFloat l h x) = RangedFloat l h (min h . max l $ negate x)
+  signum (RangedFloat l h x) = RangedFloat l h (min h . max l $ signum x)
+  fromInteger i = RangedFloat ((2^^) . fst $ floatRange (0::Float)) ((2^) . snd $ floatRange (0::Float)) (fromInteger i)
+
+
+data StringEnum = StringEnum
+  { seOptions :: [JSString]
+  , seVal :: JSString
+  }
+
+instance IsString StringEnum where
+  fromString s = let t = fromString s in StringEnum [t] t
+
+
+
