475d2434104589d57ad8a207b5184e41f38b7ebc
achirkin <chirkin@arch.ethz.ch>
2017-02-02 18:47:19 +0100

    Adapted to Luci protocol changes and added stack support

---
+++ closure-compile-stack.sh
@@ -0,0 +1,17 @@
+# minify the code
+echo "var global = this;" > web/qua-view.js
+echo "function runQuaView(){" >> web/qua-view.js
+closure-compiler --warning_level=QUIET\
+                 --language_in=ECMASCRIPT5\
+                 --compilation_level=ADVANCED_OPTIMIZATIONS\
+                 --define='DEBUG=false'\
+                 --externs=web/numeric.min.js\
+                 --externs=jsbits/qua-server-externs.js\
+ $(stack path --dist-dir)/build/qua-view/qua-view.jsexe/all.js >> web/qua-view.js
+echo "}" >> web/qua-view.js
+echo "window.onload = runQuaView.bind(this);" >> web/qua-view.js
+
+# copy qua-view.js to qua-server if possible
+if [ -d "../qua-server/static/js" ] ; then
+    cp web/qua-view.js ../qua-server/static/js/qua-view.js
+fi
