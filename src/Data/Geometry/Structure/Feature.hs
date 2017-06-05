8d7bfcc2caccb6b6cb13b96dabd5fc7ddc321a97
woenardi <jonathanmulyawan@gmail.com>
2017-06-05 17:32:08 +0800

    Fixed wrong syntax

--- src/Data/Geometry/Structure/Feature.hs
+++ src/Data/Geometry/Structure/Feature.hs
@@ -416,5 +416,5 @@ foreign import javascript unsafe "$r = {}; $r['geometry'] = $1"
 foreign import javascript unsafe "$1['geometry']['features']"
     js_GIToJSArray :: GeometryInput -> JS.Array Feature
 {-# INLINE js_JSArrayToGI #-}
-foreign import javascript unsafe "$r = {}; $r['type'] = 'FeatureCollection'; $r['features'] = $1; $s = {}; s['geometry'] = $r"
+foreign import javascript unsafe "$r = {}; $r['geometry'] = {}; $r['geometry']['type'] = 'FeatureCollection'; $r['geometry']['features'] = $1;"
     js_JSArrayToGI :: JS.Array Feature -> GeometryInput
