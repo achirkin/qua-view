e08c8da8eaa538cb9d8d9577a644a15b547e5367
achirkin <chirkin@arch.ethz.ch>
2017-07-24 19:02:17 +0200

    Added some basic widgets and removed some of obsolette stuff

--- src/Main.hs
+++ src/Main.hs
@@ -10,56 +10,43 @@ import Control.Monad.IO.Class (liftIO)
 
 import Reflex.Dom
 import Reflex.Dom.Widget.Animation (resizeEvents)
-import qualified Reflex.Class as Reflex
 
 import CommonTypes
 
 import           Widgets.Generation
 import qualified Widgets.LoadingSplash  as Widgets
 import qualified Widgets.Canvas         as Widgets
-import qualified Widgets.ControlButtons as Widgets ()
+import qualified Widgets.ControlPanel   as Widgets
 
 import qualified SmallGL
 
 main :: IO ()
-main = mainWidgetInElementById "qua-view-widgets" $ do
-  -- Change the state of the program
-  (isProgramBusy, setIsProgramBusy) <- newTriggerEvent
-  (isStartedUp, doStartUp ) <- newTriggerEvent
-  -- register loading splash so that we can change its visibility
-  Widgets.loadingSplashD isProgramBusy
-  -- add canvas element
-  -- Note: we have to create canvas before "main build" block,
-  --       so that registerAnimationHandler happens after canvas settles down;
-  --       this is important to calculate canvas size correctly
-  canvas <- Widgets.getWebGLCanvas
-  -- Main build block.
-  -- We assume that building can take a while, so we run a special "loading splash"
-  -- during this block execution.
-  ((), mainBuilt) <- Reflex.runWithReplace (liftIO $ doStartUp ()) . (<$ isStartedUp) $ mdo
+main = mainWidgetInElementById "qua-view-widgets" $ mdo
+    -- Change the state of the program
+    (isProgramBusy, setIsProgramBusy) <- newTriggerEvent
 
+    -- register loading splash so that we can change its visibility
+    Widgets.loadingSplashD isProgramBusy
 
-      el "p" $ text "Reflex is:"
-      el "ul" $ do
-        el "li" $ text "Efficient"
-        el "li" $ text "Higher-order"
-        el "li" $ text "Glitch-free"
+    -- register canvas element
+    canvas <- Widgets.getWebGLCanvas
 
-      -- initialize WebGL rendering context
-      let smallGLESel :: forall t a . Reflex t => SmallGL.SmallGLInput a -> Event t a
-          smallGLESel SmallGL.ViewPortResize = resizeEvents aHandler
+    -- add the control panel to the page
+    _panelStateD <- Widgets.controlPanel
 
-      renderingApi <- SmallGL.createRenderingEngine canvas (EventSelector smallGLESel)
-      -- initialize animation handler (and all pointer events).
-      aHandler <- Widgets.registerAnimationHandler canvas (SmallGL.render renderingApi)
+    -- initialize WebGL rendering context
+    let smallGLESel :: forall t a . Reflex t => SmallGL.SmallGLInput a -> Event t a
+        smallGLESel SmallGL.ViewPortResize = resizeEvents aHandler
+
+    renderingApi <- SmallGL.createRenderingEngine canvas (EventSelector smallGLESel)
+    -- initialize animation handler (and all pointer events).
+    aHandler <- Widgets.registerAnimationHandler canvas (SmallGL.render renderingApi)
 
 
 
-      Widgets.play aHandler
-
-
-  -- Notify everyone that the program h finished starting up now
-  performEvent_ $ liftIO (setIsProgramBusy Idle) <$ mainBuilt
+    -- Notify everyone that the program h finished starting up now
+    mainBuilt <- getPostBuild
+    performEvent_ $ (liftIO (setIsProgramBusy Idle) >> Widgets.play aHandler) <$ mainBuilt
 
 
 -- | Create a global css splice.
@@ -87,9 +74,9 @@ $(do
         padding: 0
         margin: 0
         z-index: 1
-        overflow: hidden
-        width: 100%
-        height: 100%
+        overflow: visible
+        width: 0
+        height: 0
     |]
   return []
  )
