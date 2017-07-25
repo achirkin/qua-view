1c5463ffa3565060c88e74c50695cd1d585debd3
achirkin <achirkin@users.noreply.github.com>
2017-07-25 10:29:39 +0200

    Make qua-view development more cross-platform

--- closure-compile.sh
+++ closure-compile.sh
@@ -1,16 +1,34 @@
+#!/bin/sh
+BUILD_FOLDER=$(stack path --dist-dir)/build/qua-view/qua-view.jsexe
+
 # minify the code
-echo "var global = this;" > web/qua-view.js
-echo "function runQuaView(){" >> web/qua-view.js
-closure-compiler --warning_level=QUIET\
-                 --language_in=ECMASCRIPT5\
-                 --compilation_level=ADVANCED_OPTIMIZATIONS\
-                 --define='DEBUG=false'\
-                 --externs= $(stack path --dist-dir)/build/qua-view/qua-view.jsexe/all.js.externs\
- $(stack path --dist-dir)/build/qua-view/qua-view.jsexe/all.js >> web/qua-view.js
-echo "}" >> web/qua-view.js
-echo "window.onload = runQuaView.bind(this);" >> web/qua-view.js
+cat << EOF > web/qua-view.js
+var global = this;
+function runQuaView(){
+"use strict"
+EOF
+closure-compiler --warning_level=QUIET \
+                 --language_in=ECMASCRIPT5 \
+                 --compilation_level=ADVANCED_OPTIMIZATIONS \
+                 --define='DEBUG=false' \
+                 --externs=$BUILD_FOLDER/all.js.externs \
+                 $BUILD_FOLDER/all.js >> web/qua-view.js
+cat << EOF >> web/qua-view.js
+}
+if (document.readyState === 'complete') {
+  runQuaView.bind(global)();
+} else {
+  window.onload = runQuaView.bind(global);
+}
+EOF
+
 
 # copy qua-view.js to qua-server if possible
 if [ -d "../qua-server/static/js" ] ; then
     cp web/qua-view.js ../qua-server/static/js/qua-view.js
 fi
+# copy qua-view.css to qua-server if possible
+if [ -d "../qua-server/static/css" ] ; then
+    cp web/qua-view.css ../qua-server/static/css/qua-view.css
+fi
+
