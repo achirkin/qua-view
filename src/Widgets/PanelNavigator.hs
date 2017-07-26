1cb6a0fde390ad4bd7eb4848c114a8eeddf9d7ea
woenardi <jonathanmulyawan@gmail.com>
2017-07-26 11:17:08 +0800

    Add nav wrapper for html element of panel navigator

--- src/Widgets/PanelNavigator.hs
+++ src/Widgets/PanelNavigator.hs
@@ -17,15 +17,16 @@ panelNavigator :: Reflex t => Widget x ()
 panelNavigator = do
     void $ makeElementFromHtml def $(qhtml
         [hamlet|
-          <ul class="nav nav-justified">
-            <li class="active">
-              <a aria-expanded="true" class="waves-attach waves-effect" data-toggle="tab" href="#itabGeometry">
-                Geometry
-            <li class="">
-              <a aria-expanded="false" class="waves-attach waves-effect" data-toggle="tab" href="#itabInfo">
-                Info
-            <li class="">
-              <a aria-expanded="false" class="waves-attach waves-effect" data-toggle="tab" href="#itabServices">
-                Services
-          <div style="left: 0px; right: 412px;" class="tab-nav-indicator">
+          <nav class="tab-nav tab-nav-red margin-top-no">
+            <ul class="nav nav-justified">
+              <li class="active">
+                <a aria-expanded="true" class="waves-attach waves-effect" data-toggle="tab" href="#itabGeometry">
+                  Geometry
+              <li class="">
+                <a aria-expanded="false" class="waves-attach waves-effect" data-toggle="tab" href="#itabInfo">
+                  Info
+              <li class="">
+                <a aria-expanded="false" class="waves-attach waves-effect" data-toggle="tab" href="#itabServices">
+                  Services
+            <div style="left: 0px; right: 412px;" class="tab-nav-indicator">
         |])
