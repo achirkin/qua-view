43ce7487f4ba5f8296cacc5eeaa86f41bf2986d1
woenardi <jonathanmulyawan@gmail.com>
2017-08-08 18:39:00 +0800

    Correct html for panelTabs

--- src/Widgets/Tabs.hs
+++ src/Widgets/Tabs.hs
@@ -8,7 +8,6 @@ module Widgets.Tabs
 
 import Reflex.Dom
 
-import Commons
 import Widgets.Generation
 import Widgets.Tabs.Navigator
 import Widgets.Tabs.Geometry
@@ -17,12 +16,14 @@ import Widgets.Tabs.Services
 
 panelTabs :: Reflex t => Widget x (EventSelector t GeometryTabOutE)
 panelTabs = do
-    panelStateD <- tabsNavigator
-    elClass "div" tabContent $ do
-      panelGeometryEvents <- panelGeometry panelStateD
-      panelInfo panelStateD
-      panelServices panelStateD
-      return panelGeometryEvents
+    elAttr "div" ("style" =: "overflow-y: auto; overflow-x: hidden; height: 100%;") $ do
+      elAttr "div" ("style" =: "margin: 0; padding: 0; height: 56px;") blank
+      panelStateD <- tabsNavigator
+      elClass "div" tabContent $ do
+        panelGeometryEvents <- panelGeometry panelStateD
+        panelInfo panelStateD
+        panelServices panelStateD
+        return panelGeometryEvents
   where
     tabContent = $(do
         tabContentClass <- newVar
