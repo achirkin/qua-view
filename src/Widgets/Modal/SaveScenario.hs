3cd97356ac7f5ad275c640f4eccda1de97bf8848
achirkin <chirkin@arch.ethz.ch>
2017-08-15 21:12:03 +0200

    cleaned geometry tabs a little bit

--- src/Widgets/Modal/SaveScenario.hs
+++ src/Widgets/Modal/SaveScenario.hs
@@ -14,23 +14,23 @@ import Widgets.Modal
 
 
 -- | Event happening when user clicks on save scenario button.
---   TODO: probably, the type of scenario name should be different and provided by luci.
-newtype UserAsksSaveScenario = UserAsksSaveScenario JSString
+newtype UserAsksSaveScenario = UserAsksSaveScenario Text
   deriving (Eq, Show)
 
 popupSaveScenario :: Reflex t => Event t (ElementClick saveScenarioButton) -> Widget x (Event t UserAsksSaveScenario)
 popupSaveScenario savePopupE = snd <$> createModalWithClicks savePopupE Inactive saveScenarioContent
 
+-- TODO: set input value to Nothing on save to reset the field.
 saveScenarioContent :: Reflex t => Widget x (Event t (ElementClick "cancel save scenario"), Event t UserAsksSaveScenario)
 saveScenarioContent = do
   elClass "div" "modal-heading" $
     elClass "p" "modal-title" $ text "Enter a name for a new scenario to save it on a server"
-  _scNameE <- elClass "div" "modal-inner" $ -- TODO: transform this into behavior and then apply on save event
+  scNameI <- elClass "div" "modal-inner" $
     elClass "div" "form-group form-group-label" $ do
       elAttr "label" (("class" =: "floating-label") <> ("for" =: "save-scenario-name-input")) $ text "Scenario name"
       textInput $ def & attributes .~ constDyn (("class" =: "form-control") <> ("id" =: "save-scenario-name-input"))
   elClass "div" "modal-footer" $
     elClass "p" "text-right" $ do
       ce <- buttonFlat "Cancel" def
-      se <- buttonFlat "Save" def -- TODO: Save scenario
-      return (ce, UserAsksSaveScenario undefined <$ se)
+      se <- buttonFlat "Save" def
+      return (leftmost [ce, se], UserAsksSaveScenario <$> current (_textInput_value scNameI) <@ se)
--- src/Widgets/Tabs/Geometry.hs
+++ src/Widgets/Tabs/Geometry.hs
@@ -45,10 +45,10 @@ data GeometryTabOutE e where
 
 
 panelGeometry :: forall t x . Reflex t => EventSelector t CompState -> Widget x (EventSelector t GeometryTabOutE)
-panelGeometry compStateEvs = do
+panelGeometry _compStateEvs = do
 
     el "div" $ text "Read GeoJSON from file"
-    clearGeometryClicked <-
+    clearGeometryClickedE <-
       el "div" $ do
         -- clear geometry button
         cgClicked <- buttonRed @"ClearGeometry" "Clear" def
@@ -59,11 +59,13 @@ panelGeometry compStateEvs = do
 
         return cgClicked
 
-    luciScenarios
+    (asksSaveScenarioE, asksSelectScenarioE) <- luciScenarioPane
 
     -- combine all outgoing events together
     let outEvsSel :: forall a . GeometryTabOutE a -> Event t a
-        outEvsSel GeomOutUserAsksClearGeometry = clearGeometryClicked
+        outEvsSel GeomOutUserAsksClearGeometry = clearGeometryClickedE
+        outEvsSel GeomOutUserAsksSaveScenario  = asksSaveScenarioE
+        outEvsSel GeomOutUserSelectedScenario  = asksSelectScenarioE
 
     return $ EventSelector outEvsSel
 
@@ -121,77 +123,27 @@ foreign import javascript unsafe "((typeof $1 === 'string') ? $1 : null)"
     hopeForString :: JSFFI.StringOrArrayBuffer -> JSFFI.Nullable JSString
 
 
+-- | User indicates that they want to save scanerio with a specified name (via pop-up)
+buttonSaveScenario :: Reflex t => Widget x (Event t UserAsksSaveScenario)
+buttonSaveScenario = buttonRed "Save" def >>= popupSaveScenario
+
+-- | User selects
+buttonBrowseScenarios :: Reflex t => Widget x (Event t UserSelectedScenario)
+buttonBrowseScenarios = buttonRed "Scenarios" def >>= popupBrowseScenarios
 
 
 
-
-
-fileUploadGeometry :: Reflex t => Widget x (Event t (ElementClick "ClearGeometry"), Event t JSString)
-fileUploadGeometry = do
-  el "div" $ text "Read GeoJSON from file"
-  el "div" $ do
-    clearGeometryBtn <- buttonRed "Clear" def -- TODO: Clear Geometry
-
-    fInput <- fileInput def
-    _filesBtn <- buttonRed "Files" def -- TODO: Connect this with jsonFileInput for file uploading
-    fileNameIndicator $ constDyn "" -- TODO: Dynamic text correspond with the uploaded file name
-    jsonFileInput -- TODO: file uploadings
-
-    return (clearGeometryBtn, never)
-
-
-
-fileNameIndicator :: Reflex t => Dynamic t Text -> Widget x ()
-fileNameIndicator = elAttr "div" attrs . dynText
-  where
-    attrs = "style" =: "display: inline; font-size: 0.9em;"
-
-jsonFileInput :: Reflex t => Widget x ()
-jsonFileInput = elAttr "input" attrs blank
-  where
-    attrs = ("style" =: "display:none") <> ("type" =: "file")
-
--- Luci Scenario
-
-data LuciState = Connected | Disconnected
-  deriving Eq
-
-luciScenarios :: Reflex t => Widget x (EventSelector t GeometryTabOutE)
-luciScenarios = do
-    let luciState = constDyn Connected -- Placeholder
-    elDynAttr "div" (attrs1 <$> luciState) $
-      el "div" $ text "Luci scenarios are not available"
-    elDynAttr "div" (attrs2 <$> luciState) luciScenarioPane
-  where
-    attrs1 state = "style" =: ("display: " <> display1 state)
-    attrs2 state = "style" =: ("display: " <> display2 state)
-    display1 Connected = "none"
-    display1 Disconnected = "block"
-    display2 Connected = "block"
-    display2 Disconnected = "none"
-
-luciScenarioPane :: forall t x . Reflex t => Widget x (EventSelector t GeometryTabOutE)
+luciScenarioPane :: Reflex t
+                 => Widget x ( Event t UserAsksSaveScenario
+                             , Event t UserSelectedScenario )
 luciScenarioPane = do
   el "div" $ text "Remote (Luci scenarios)"
   el "div" $ do
-    userSelectedScenarioE <- browseScenariosWidget
-    userWantsToSaveScenarioE <- saveScenarioWidget $ constDyn True -- TODO: Hide this button when there is no active scenario
-    fileNameIndicator $ constDyn "placeholder" -- TODO: Dynamic text correspond with saved file name
-    let selectorFun :: forall e . GeometryTabOutE e -> Event t e
-        selectorFun GeomOutUserSelectedScenario = userSelectedScenarioE
-        selectorFun GeomOutUserAsksSaveScenario = userWantsToSaveScenarioE
-    return $ EventSelector selectorFun
+    userSelectedScenarioE    <- buttonBrowseScenarios
+    userWantsToSaveScenarioE <- buttonSaveScenario
+    performEvent_ $ (\(UserAsksSaveScenario r) -> liftIO $ print r) <$> userWantsToSaveScenarioE
+    -- TODO: fileNameIndicator $ constDyn "placeholder" -- TODO: Dynamic text correspond with saved file name
+    return (userWantsToSaveScenarioE, userSelectedScenarioE)
 
 
-browseScenariosWidget :: Reflex t => Widget x (Event t UserSelectedScenario)
-browseScenariosWidget = buttonRed "Scenarios" def >>= popupBrowseScenarios
 
-saveScenarioWidget :: Reflex t => Dynamic t Bool -> Widget x (Event t UserAsksSaveScenario)
-saveScenarioWidget scenarioActive = do
-  (e, _) <- elDynAttr' "a" (attrs <$> scenarioActive) $ text "Save"
-  popupSaveScenario $ ElementClick <$ domEvent Click e
-  where
-    attrs active = ("class" =: "btn btn-red waves-attach waves-light waves-effect")
-                <> ("style" =: ("margin: 2px; display: " <> displayClass active))
-    displayClass True  = "inline"
-    displayClass False = "none"
