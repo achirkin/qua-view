27a593fa8c83a58a01b771e7025b13d4712ab53b
achirkin <achirkin@users.noreply.github.com>
2017-08-25 10:13:18 +0200

    Cleaned up all older files (from master branch).

--- src/Model/GeoJSON/Coordinates.hs
+++ src/Model/GeoJSON/Coordinates.hs
@@ -22,9 +22,8 @@
 --
 module Model.GeoJSON.Coordinates
     ( PaddedZeros (..)
---    , PointSeq (..), LinearRing (..), LinearRingWithNormals (..)
---    , bestFittingPlaneN, bestFittingLineN
---    , varX, meanX
+    , bestFittingPlaneN, bestFittingLineN
+    , varX, meanX
     , ObjectCentres (..)
     , getScenarioStatistics
     ) where
@@ -43,161 +42,67 @@ import Unsafe.Coerce
 import Model.Scenario.Statistics
 import Model.Scenario.Object.Geometry
 
+-- | Flag telling if fromJSON parsing function appened zeros as third coordinates
+--   to all vertices of a Geometry.
 newtype PaddedZeros = PaddedZeros Bool
   deriving (Eq, Show)
 
----- | 4xB where N >= 2
---newtype PointSeq = PointSeq (SomeIODataFrame Float '[N 4, XN 2])
---  deriving Eq
---
----- | 4xN where N >= 3 (last point removed)
---newtype LinearRing = LinearRing (SomeIODataFrame Float '[N 4, XN 3])
---  deriving Eq
---
----- | 4x2xN where N >= 3 (last point removed)
---newtype LinearRingWithNormals = LinearRingWithNormals (DataFrame Float '[N 4, N 2, XN 3])
---  deriving Eq
---
---
----- https://math.stackexchange.com/q/2306029
---bestFittingPlaneN :: DataFrame Float '[N 4, XN 3] -> Vec3f
---bestFittingPlaneN (SomeDataFrame (df' :: DataFrame Float ns))
---    | (Evidence :: Evidence ([4,n] ~ ns, 2 <= n)) <- unsafeCoerce (Evidence :: Evidence ())
---      -- take mean
---    , m  <- meanX df'
---      -- normalized frame
---    , df <- ewmap   @_ @'[4] (flip (-) m) df'
---      -- solve Ax = B
---    , b  <- ewmap @Float @'[] (3:!Z !. ) df :: DataFrame Float '[n]
---    , a  <- ewmap @Float @'[3] @'[n] @'[3,n]
---                  @Float @'[4]       @'[4,n]
---                  (\v -> vec3 (unScalar $ 1:!Z !. v) (unScalar $ 2:!Z !. v) 1 ) df
---    , aT <- transpose a :: DataFrame Float '[n, 3]
---    , aX <- inverse $ a %* aT :: DataFrame Float '[3, 3]
---    , r3 <- aX %* a %* b
---    , df2d <- ewmap (\v -> vec2 (unScalar $ 1:!Z !. v) (unScalar $ 2:!Z !. v) ) df
---    = if abs (det aX) > 0.001
---      then let v = vec3 (unScalar $ 1 !. r3) (unScalar $ 2 !. r3) 1 in v / fromScalar (normL2 v)
---      else bestFittingLineN df2d <+:> 0
---
----- Here we assume it is already normalized
---bestFittingLineN :: forall n . (KnownDim n, 2 <= n) => DataFrame Float '[2, n] -> Vec2f
---bestFittingLineN df
---    | -- solve Ax = B
---      b  <- ewmap @Float @'[]  @'[n] @'[n]    (2:!Z !. ) df
---    , a  <- ewmap @Float @'[2] @'[n] @'[2,n]
---                  @Float @'[2]       @'[2,n]
---                  (\v -> vec2 (unScalar $ 1:!Z !. v) 1 ) df
---    , aT <- transpose a :: DataFrame Float '[n, 2]
---    , aX <- inverse $ a %* aT :: DataFrame Float '[2, 2]
---    , r2  <- aX %* a %* b
---    , (vx, vy) <- unpackV2 $ varX df
---    = if vx < 0.001 || vx / vy < 0.00001
---      then vec2 0 1
---      else if vy < 0.001 || vy / vx < 0.00001
---           then vec2 1 0
---           else let v = vec2 (unScalar $ 1 !. r2) 1 in v / fromScalar (normL2 v)
---
---meanX :: forall n m . (KnownDim n, KnownDim m) => DataFrame Float '[n, m] -> Vector Float n
---meanX x = ewfoldl (+) 0 x / fromIntegral (dimVal' @m)
---
---varX :: forall n m . (KnownDim n, KnownDim m) => DataFrame Float '[n, m] -> Vector Float n
---varX x = ewfoldl (\a v -> let v' = (v - m) in a + v' * v' ) 0 x / fromIntegral (dimVal' @m - 1)
---  where
---    m = meanX x
---
---
---
---instance FromJSON (IODataFrame Float '[3], PaddedZeros) where
---    parseJSON val = flip (withArray "Vec3f") val $ \coords ->
---       pure . second PaddedZeros $ js_parseVec3f coords
---
---instance FromJSON (IODataFrame Float '[4], PaddedZeros) where
---    parseJSON val = flip (withArray "Vec4f") val $ \coords ->
---       pure . second PaddedZeros $ js_parseVec4f coords
---
---instance FromJSON (PointSeq, PaddedZeros) where
---    parseJSON val = flip (withArray "Point sequence") val $ \coords ->
---       let lvl = js_arrayNestingLvl coords
---       in if lvl /= 2
---          then fail $ "Array nesting level must be 2, but encountered " ++ show lvl ++ "."
---          else let (jsv, len, pz) = js_parsePointSeq coords
---               in if len >= 2
---                  then case someIntNatVal len of
---                    Nothing -> fail $
---                       "Could not set DataFrame length: " ++ show len ++ "."
---                    Just (SomeIntNat (_ :: Proxy n)) ->
---                       pure (PointSeq $ SomeDataFrame (unsafeCoerce jsv :: DataFrame Float '[4,n])
---                                                      , PaddedZeros pz)
---                  else fail $
---                    "Length of a point array must be greater than 1, but the array length is "
---                      ++ show len ++ "."
---
---instance FromJSON (LinearRing, PaddedZeros) where
---    parseJSON val = flip (withArray "Linear ring") val $ \coords ->
---       let lvl = js_arrayNestingLvl coords
---       in if lvl /= 2
---          then fail $ "Array nesting level must be 2, but encountered " ++ show lvl ++ "."
---          else let (jsv, len, pz) = js_parseLinearRing coords
---               in if len >= 3
---                  then case someIntNatVal len of
---                    Nothing -> fail $
---                       "Could not set DataFrame length: " ++ show len ++ "."
---                    Just (SomeIntNat (_ :: Proxy n)) ->
---                       pure (LinearRing $ SomeDataFrame (unsafeCoerce jsv :: DataFrame Float '[4,n])
---                                                        , PaddedZeros pz)
---                  else fail $
---                    "Length of a linear ring must be greater than 3, but the array length is "
---                      ++ show (len+1) ++ "."
---
-----instance FromJSON (Polygon, PaddedZeros) where
-----    parseJSON val = flip (withArray "Polygon") val $ \coords ->
-----       let lvl = js_arrayNestingLvl coords
-----       in if lvl /= 3
-----          then fail $ "Array nesting level must be 3, but encountered " ++ show lvl ++ "."
-----          else let (jsv, len, pz, holes) = js_parsePolygon coords
-----               in if len >= 1
-----                  then case someIntNatVal len of
-----                    Nothing -> fail $
-----                       "Could not set DataFrame length: " ++ show len ++ "."
-----                    Just (SomeIntNat (_ :: Proxy n)) -> pure
-----                       ( Polygon (SomeDataFrame (unsafeCoerce jsv :: DataFrame Float '[4,n]))
-----                                 (pFromJSVal <$> JSArray.toList holes)
-----                       , PaddedZeros pz)
-----                  else fail "A polygon must have at least one linear ring inside"
+----------------------------------------------------------------------------------------------------
+-- * Some math
+----------------------------------------------------------------------------------------------------
 
 
+-- https://math.stackexchange.com/q/2306029
+bestFittingPlaneN :: DataFrame Float '[N 4, XN 3] -> Vec3f
+bestFittingPlaneN (SomeDataFrame (df' :: DataFrame Float ns))
+    | (Evidence :: Evidence ([4,n] ~ ns, 2 <= n)) <- unsafeCoerce (Evidence :: Evidence ())
+      -- take mean
+    , m  <- meanX df'
+      -- normalized frame
+    , df <- ewmap   @_ @'[4] (flip (-) m) df'
+      -- solve Ax = B
+    , b  <- ewmap @Float @'[] (3:!Z !. ) df :: DataFrame Float '[n]
+    , a  <- ewmap @Float @'[3] @'[n] @'[3,n]
+                  @Float @'[4]       @'[4,n]
+                  (\v -> vec3 (unScalar $ 1:!Z !. v) (unScalar $ 2:!Z !. v) 1 ) df
+    , aT <- transpose a :: DataFrame Float '[n, 3]
+    , aX <- inverse $ a %* aT :: DataFrame Float '[3, 3]
+    , r3 <- aX %* a %* b
+    , df2d <- ewmap (\v -> vec2 (unScalar $ 1:!Z !. v) (unScalar $ 2:!Z !. v) ) df
+    = if abs (det aX) > 0.001
+      then let v = vec3 (unScalar $ 1 !. r3) (unScalar $ 2 !. r3) 1 in v / fromScalar (normL2 v)
+      else bestFittingLineN df2d <+:> 0
 
+-- Here we assume it is already normalized
+bestFittingLineN :: forall n . (KnownDim n, 2 <= n) => DataFrame Float '[2, n] -> Vec2f
+bestFittingLineN df
+    | -- solve Ax = B
+      b  <- ewmap @Float @'[]  @'[n] @'[n]    (2:!Z !. ) df
+    , a  <- ewmap @Float @'[2] @'[n] @'[2,n]
+                  @Float @'[2]       @'[2,n]
+                  (\v -> vec2 (unScalar $ 1:!Z !. v) 1 ) df
+    , aT <- transpose a :: DataFrame Float '[n, 2]
+    , aX <- inverse $ a %* aT :: DataFrame Float '[2, 2]
+    , r2  <- aX %* a %* b
+    , (vx, vy) <- unpackV2 $ varX df
+    = if vx < 0.001 || vx / vy < 0.00001
+      then vec2 0 1
+      else if vy < 0.001 || vy / vx < 0.00001
+           then vec2 1 0
+           else let v = vec2 (unScalar $ 1 !. r2) 1 in v / fromScalar (normL2 v)
 
+meanX :: forall n m . (KnownDim n, KnownDim m) => DataFrame Float '[n, m] -> Vector Float n
+meanX x = ewfoldl (+) 0 x / fromIntegral (dimVal' @m)
 
-
---
---foreign import javascript unsafe
---    "var a = h$geojson_parseVec4($1); $r1 = a[0]; $r2 = a[1];"
---    js_parseVec4f :: JSArray -> (IODataFrame Float '[4], Bool)
---
---foreign import javascript unsafe
---    "var a = h$geojson_parsePointSeq($1); $r1 = a[0]; $r2 = a[1]; $r3 = a[2];"
---    js_parsePointSeq :: JSArray -> (JSVal, Int, Bool)
---
---foreign import javascript unsafe
---    "var a = h$geojson_parseLinearRing($1); $r1 = new Float32Array(a[0]); $r2 = a[1]; $r3 = a[2];"
---    js_parseLinearRing :: JSArray -> (JSVal, Int, Bool)
---
---foreign import javascript unsafe
---    "var a = h$geojson_parsePolygon($1);$r1 = new Float32Array(a[0]);$r2 = a[1];$r3 = a[2];$r4 = a[3];"
---    js_parsePolygon :: JSArray -> (JSVal, Int, Bool, JSArray)
---
---foreign import javascript unsafe
---    "h$geojson_nestingLvl($1)"
---    js_arrayNestingLvl :: JSArray -> Int
+varX :: forall n m . (KnownDim n, KnownDim m) => DataFrame Float '[n, m] -> Vector Float n
+varX x = ewfoldl (\a v -> let v' = (v - m) in a + v' * v' ) 0 x / fromIntegral (dimVal' @m - 1)
+  where
+    m = meanX x
 
 
 ----------------------------------------------------------------------------------------------------
 -- * Converting from JSON
 ----------------------------------------------------------------------------------------------------
-
-
 
 
 instance FromJSON (Geometry, PaddedZeros) where
