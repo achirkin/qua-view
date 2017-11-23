a1ad6c31e0549225becddbfaac96b9cd4034a70b
achirkin <achirkin@users.noreply.github.com>
2017-11-23 10:13:14 +0100

    separated camera behavior into Program module branch

--- src/Model/Camera.hs
+++ src/Model/Camera.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RecursiveDo           #-}
 {-# LANGUAGE ViewPatterns          #-}
 module Model.Camera
     ( Camera (..)
@@ -10,112 +9,15 @@ module Model.Camera
     , initCamera
     , scroll, dragHorizontal, dragVertical, rotateCentered, twoFingerControl
     , dragObject, rotateObject, twoFingerObject
-    , dynamicCamera
---    , cameraBehavior
---    , ObjectTransform (..), objectTransformEvents
+    , makeProjM
     ) where
 
 import           Data.Fixed                  (mod')
-
-import           Reflex.Class
-import           Reflex.Dom.Widget.Animation (AEventType (..), PEventType (..), AnimationHandler )
-import qualified Reflex.Dom.Widget.Animation as A
 
 import           Numeric.DataFrame
 --import Numeric.Dimensions
 import qualified Numeric.Matrix              as Matrix
 import qualified Numeric.Quaternion          as Q
-
-import           Commons
-
-
-
-----------------------------------------------------------------------------------------------
--- Definitions -------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------------
-
--- | Reflex camera's behavior
-dynamicCamera :: ( Reflex t, MonadHold t m, MonadFix m )
-              => Camera
-              -> AnimationHandler t
-              -> Event t () -- ^ reset camera event
-              -> Behavior t Vec2f
-              -> m (Dynamic t Camera)
-dynamicCamera icam aHandler resetCameraE defCenterPointB = mdo
-    -- all changes of the viewport size come from AResizeEvent (or resetCameraE)
-    viewportSizeD <- holdDyn (viewportSize icam) $ leftmost [resizeE, viewportSize icam <$ resetCameraE]
-    -- projection matrix also changes only at AResizeEvents (or resetCameraE)
-    projMatrixD <- holdDyn (projMatrix icam) $ leftmost [makeProjM <$> resizeE, projMatrix icam <$ resetCameraE]
-    -- checkpoint camera states - when there are no active actions, such as mouse dragging
-    oldStateD <- holdDyn (oldState icam) $ leftmost
-        [ -- mouse wheel is an atomic event, so we update oldState directly
-          flip wheelT <$> camB <@> select (A.animationEvents aHandler) AWheelEvent
-          -- copy newState when some continuous action finishes
-        , newState <$> camB <@ select (A.animationEvents aHandler) (APointerEvent PUpEvent)
-        , newState <$> camB <@ select (A.animationEvents aHandler) (APointerEvent PClickEvent)
-        , newState <$> camB <@ select (A.animationEvents aHandler) (APointerEvent PCancelEvent)
-          -- should not be needed, but also should not harm?
-        , newState <$> camB <@ select (A.animationEvents aHandler) (APointerEvent PDownEvent)
-          -- reset camera
-        , (\cp -> (oldState icam){ viewPoint = cp <+:> 0 } ) <$> defCenterPointB <@ resetCameraE
-        ]
-    -- updates on every action
-    newStateD <- holdDyn (newState icam) $ leftmost
-        [ -- every time oldState updates, copy its state
-          updated oldStateD
-          -- get pairs of screen points (old-new) on each mouseDrag event
-        , flip ($) <$> camB <@> fmapMaybe id movs
-        ]
-
-    let camD = Camera <$> viewportSizeD
-                      <*> projMatrixD
-                      <*> oldStateD
-                      <*> newStateD
-        camB = current camD
-
-    return camD
-  where
-    -- resize events in local format
-    resizeE = (\(A.ResizeEvent s) -> realToFrac *** realToFrac $ s)
-              <$> select (A.animationEvents aHandler) AResizeEvent
-
-    -- Modify camera with will zooming
-    wheelT :: A.WheelEvent -> Camera -> CState
-    wheelT A.WheelUp   = scroll (-0.1)
-    wheelT A.WheelDown = scroll 0.15
-
-    -- All pointer move events
-    pointerT :: Int -> [(Vec2f,Vec2f)] -> Maybe (Camera -> CState)
-    -- move unknown move (should not happen anyway)
-    pointerT _ [] = Nothing
-    -- Three-finger rotation
-    pointerT 1 ((o1,n1):_:_:_)  = Just $ rotateCentered o1 n1
-    -- Complicated two-finger control
-    pointerT 1 [(o1,n1),(o2,n2)] = Just $ twoFingerControl (o1,o2) (n1,n2)
-    -- Mouse control             -- do nothing if no button pressed
-    pointerT b ((opos,npos):_) | b == 0 = Nothing
-                                 -- Drag horizontally using left mouse button
-                               | b == 1 = Just $ dragHorizontal opos npos
-                                 -- Rotating using secondary button (right m b)
-                               | b == 2 = Just $ rotateCentered opos npos
-                                 -- Dragging vertically using wheel button press
-                               | b == 4 = Just $ dragVertical opos npos
-                                 -- fallback to horizontal dragging
-                               | otherwise = Just $ dragHorizontal opos npos
-
-    dTupleToVec :: (Double, Double) -> Vec2f
-    dTupleToVec (x,y) = vec2 (realToFrac x) (realToFrac y)
-
-
-    -- movs :: Event t (Camera -> CState)
-    -- get camera motion updates
-    movs = (\b as bs -> pointerT b $ zipWith (\o n -> (dTupleToVec o, dTupleToVec n)) as bs)
-          <$> A.buttonsB aHandler
-          <*> A.downPointersB aHandler
-          <*> A.curPointersB aHandler
-          <@ select (A.animationEvents aHandler) (APointerEvent PMoveEvent)
-
-
 
 ----------------------------------------------------------------------------------------------
 -- Definitions -------------------------------------------------------------------------------
