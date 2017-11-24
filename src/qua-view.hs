6904d1c482dfedb6660136f6a882f02be95c6876
mb21 <mb21@users.noreply.github.com>
2017-11-24 15:33:32 +0100

    Info panel: show data of selected object

--- src/qua-view.hs
+++ src/qua-view.hs
@@ -47,7 +47,7 @@ main = mainWidgetInElementById "qua-view-widgets" $ runQuaWidget $ mdo
     canvas <- Widgets.getWebGLCanvas
 
     -- add the control panel to the page
-    _panelStateD <- Widgets.controlPanel renderingApi scenarioB
+    _panelStateD <- Widgets.controlPanel renderingApi scenarioB selectedObjIdD
 
     -- get an event of loaded geometry text, combine it with current state of scenario,
     -- and make a new event to be consumed by the LoadGeometryWorker
