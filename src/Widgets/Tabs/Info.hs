2006948ed1f3817560c5f478b944c2378ad3c934
mb21 <mb21@users.noreply.github.com>
2017-11-24 15:33:32 +0100

    Info panel: show data of selected object

--- src/Widgets/Tabs/Info.hs
+++ src/Widgets/Tabs/Info.hs
@@ -8,14 +8,13 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE RecursiveDo #-}
 
-{-# LANGUAGE TypeApplications #-}
-
 module Widgets.Tabs.Info
     ( panelInfo
     ) where
 
-import Control.Lens.Getter
+import Control.Lens
 import Control.Lens.Indexed (itraverse_)
+import Data.Maybe (fromMaybe, isJust)
 import Data.Text (pack)
 import Reflex.Dom
 import JavaScript.JSON.Types.Instances()
@@ -24,23 +23,41 @@ import qualified JavaScript.JSON.Types.Internal as GHCJS
 import Commons
 import Data.JSString.Text (textFromJSString)
 import qualified Model.Scenario as Scenario
+import qualified Model.Scenario.Object as Object
+import Model.Scenario.Object (ObjectId (..))
 import Model.Scenario.Properties
 import Program.Scenario
 import Widgets.Generation
 
-panelInfo :: Reflex t => (Behavior t Scenario.Scenario) -> QuaWidget t x ()
-panelInfo scenarioB = do
-  scenarioPropUpdated <- askEvent $ ScenarioUpdate ScenarioPropertyUpdated
-  scenarioUpdated     <- askEvent $ ScenarioUpdate ScenarioUpdated
+data InfoData = InfoData (Maybe ObjectId) Properties
+
+panelInfo :: Reflex t
+          => Behavior t Scenario.Scenario
+          -> Dynamic t (Maybe ObjectId)
+          -> QuaWidget t x ()
+panelInfo scenarioB selectedObjIdD = do
+  scenarioPropUpdatedE <- askEvent $ ScenarioUpdate ScenarioPropertyUpdated
+  scenarioUpdatedE     <- askEvent $ ScenarioUpdate ScenarioUpdated
   delayedE <- delay 0 $ leftmost --delay so we sample the behavior after its updated
-    [ () <$ scenarioPropUpdated
-    , () <$ scenarioUpdated
+    [ () <$ scenarioPropUpdatedE
+    , () <$ scenarioUpdatedE
     ]
-  let propE = view Scenario.properties <$> scenarioB <@ delayedE
+  let getScProps = view Scenario.properties
+  let getData (s, mObjId) = InfoData mObjId props
+        where
+          props = case mObjId of
+            Just objId -> fromMaybe (getScProps s) $ s ^?
+                          Scenario.objects.(at objId)._Just.Object.properties
+            Nothing    -> getScProps s
+  let scLoadedE    = Nothing <$ delayedE
+      objSelectedE = updated selectedObjIdD
+      propE = getData <$> attach scenarioB (leftmost [objSelectedE, scLoadedE])
   void $ widgetHold (return ()) (renderInfo <$> propE)
 
-renderInfo :: Reflex t => Properties -> QuaWidget t x ()
-renderInfo props = do
+renderInfo :: Reflex t => InfoData -> QuaWidget t x ()
+renderInfo (InfoData mObjId props) = do
+  when (isJust mObjId) $
+    el "p" $ text "Object selected"
   elClass "table" tableClass $ itraverse_ renderProp props
   where
     renderProp (PropName key) = propValue $ \mvval -> do
