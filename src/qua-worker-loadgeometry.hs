73e5dc01850950e7bc674bdd2bcdcb7aefefec0f
Artem Chirkin <chirkin@arch.ethz.ch>
2018-01-19 14:14:10 +0800

    Start preparing geom worker for incremental upload

--- src/qua-worker-loadgeometry.hs
+++ src/qua-worker-loadgeometry.hs
@@ -2,13 +2,11 @@ module Main ( main ) where
 
 
 import Commons.NoReflex
-import Control.Monad.Trans.State.Strict
 
 import Workers
 import Workers.LoadGeometry
 
 main :: IO ()
-main = flip evalStateT mempty
-     $ runReaderT (execWorkerConduit loadGeometryDef loadGeometryConduit) stdOutLogger
+main = runReaderT (execWorkerConduit loadGeometryDef loadGeometryConduit) stdOutLogger
 
 
